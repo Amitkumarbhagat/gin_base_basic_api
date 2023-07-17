2e6d4256e9afa2fd65ffb77911a0f5a7a2281607
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2023-07-18 00:52:45 +0530

    Get request completed let's understand about post request

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
@@ -11,4 +17,22 @@ var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Amit Bhagat", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "Sumit bhagat", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Ankit bhagat", Quantity: 6},
+}
+
+// the gin context is all the information about the request, and
+//it allows you to return a response
+
+// by indentedJson we can return file text etc. in this case returning
+// book slice in well manner
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func main() {
+	//	router responsible for handle different kind of routes and kind
+	//	of different end points of our api
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
+
 }
